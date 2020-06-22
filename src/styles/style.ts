5dc8ab8fd40d0a2f4ed9bc9efb5eca86100ff0fa
takeshi_odaka <>
2020-06-22 10:30:52 +0900

    使用しなかったスタイル定義を削除

--- src/styles/style.ts
+++ src/styles/style.ts
@@ -1,20 +1,4 @@
 // styleで使用する定数を管理する
-export const BUTTON = {
-  primary: {
-    color: 'rgb(255, 255, 255)',
-    bg: 'rgb(234, 53, 45)',
-    border: 'rgb(234, 53, 45)'
-  },
-  secondary: {
-    color: 'rgb(0,0,0)',
-    bg: '#ff0'
-  },
-  disabled: {
-    color: '#fff',
-    bg: '#ccc'
-  }
-} as const;
-
 export const COLOR = {
   link: '#f60',
   emphasis: '#ff0',
